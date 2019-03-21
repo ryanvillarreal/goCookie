62977fbf33016488ff5274cd81b0e5fc0e4729df
Ryan Villarreal <999886+ryanvillarreal@users.noreply.github.com>
2019-03-21 12:55:52 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # run this to setup goCookie.
+# Make sure to have go already installed on your machine and in the appropriate path
 
+# Need to verify this works. 
+# go get
+# go build main.go
